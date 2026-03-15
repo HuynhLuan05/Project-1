a98c659bea717c8371723c73cd9eeea4f1505de7
tnnhuaa <nguyenhientuananh@gmail.com>
2026-03-15 23:07:37 +0700

    fix: correct import statements for ObjectMapper in multiple files

--- recommendation/src/main/java/com/yas/recommendation/vector/common/document/DefaultIdGenerator.java
+++ recommendation/src/main/java/com/yas/recommendation/vector/common/document/DefaultIdGenerator.java
@@ -3,7 +3,6 @@ package com.yas.recommendation.vector.common.document;
 import java.util.UUID;
 import org.springframework.ai.document.id.IdGenerator;
 
-// TODO: currently, it will be used for all document, consider to make this overridable.
 public class DefaultIdGenerator implements IdGenerator {
 
     private final Long identity;
